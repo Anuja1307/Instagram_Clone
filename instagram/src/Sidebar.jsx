0655a5a6368d2d617001ab3b037bd6a24182481d
Anuja1307 <anujars541@gmail.com>
2026-05-22 10:18:30 +0530

    Added ui to sidebar

---
+++ instagram/src/Sidebar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <>
+    <div>
+        <div className='d-flex flex-column gap-4'> 
+                <img src="./src/assets/instagram_text.png" alt="" style={{width:'180px'}}/>
+                <div> <i class="bi bi-house-door-fill"></i> Home</div>
+                <div> <i class="bi bi-search"></i> Search</div>
+                <div> <i class="bi bi-compass"></i> Explore</div>
+                <div> <i class="bi bi-play-btn"></i> Reels</div>
+                <div><i class="bi bi-chat-dots"></i> Messages</div>
+                <div><i class="bi bi-heart"></i> Notifications</div>
+                <div><i class="bi bi-plus-square"></i>Create</div>
+                <div><i class="bi bi-person-circle"></i> Profile</div>
+         </div>
+
+          <div className='d-flex flex-column gap-3 position fixed-bottom'>
+            <div><i class="bi bi-threads"></i>Threads</div>
+            <div className='my-2'><i class="bi bi-list "></i>More</div>
+         </div>
+    </div>
+         
+
+
+    </>
+
+  )
+}
+
+export default Sidebar
